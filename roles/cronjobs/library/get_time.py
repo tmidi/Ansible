569a4cf8b5756856b3c024dc26f3db768d31ee53
Taleeb <taleebmidi@gmail.com>
2017-06-26 00:04:26 -0400

    initial commit

---
+++ roles/cronjobs/library/get_time.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python -x
+
+import json
+
+DOCUMENTATION = '''
+---
+module: get_time
+short_description: Get time based on IP address
+'''
+
+EXAMPLES = '''
+- name: Get time
+  get_time:
+    ipaddress : "10.0.0.1"
+  register: r
+'''
+
+from ansible.module_utils.basic import *
+
+
+def ip_time_min(data):
+    
+    valueip = data.split(".")
+    minimalip = "".join(valueip[2:4])
+    minutes = int(minimalip) % 60
+    return minutes
+
+
+def ip_time_hour(data):
+    
+    valueip = data.split(".")
+    minimalip = "".join(valueip[2:4])
+    hour = int(minimalip) % 24
+    return hour
+
+
+def main():
+
+    module = AnsibleModule(
+        argument_spec = dict(
+            ipaddress = dict(required=True, type="str"),
+        )
+    )
+
+    ipaddress = module.params['ipaddress']
+    
+    choice_map = {
+        "minute" : ip_time_min(ipaddress),
+        "hour" : ip_time_hour(ipaddress),
+    }
+
+    result = json.dumps(choice_map)
+    return choice_map
+
+if __name__ == '__main__':  
+    main()
